3753e0f749d90f1dee74eee27371993a06c1dbb2
josepenso <107490244+josepenso@users.noreply.github.com>
2023-02-03 14:27:15 -0800

    Update CukesRunner.java

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.testng.annotations.*;
         features = "src/test/resources/features",
         glue = "com/trycloud/stepDef",
         dryRun = false,
-        tags = "@parallel",
+        tags = "@parallel or @firefox",
         publish = true
 )
 
